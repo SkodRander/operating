dcc692d0dd6784cfb3ebf026aa28ee24d4ec1747
Frederik Rander Andersen <frederik396@gmail.com>
2018-09-27 22:42:18 +0200

    f

--- commandprogram.c
+++ commandprogram.c
@@ -4,40 +4,40 @@
 
 int *dynamic_table(int *oldarray, int *arraysize) {
 	int *newarray = NULL;
-	newarray = (int *) malloc(*arraysize * 2 * sizeof(int));   // new array = 2x size of old array
-	memcpy(newarray, oldarray, *arraysize * 2 * sizeof(int));	 // copy old array content to new array
+	newarray = (int *) malloc(*arraysize * 2 * sizeof(int)); // new array = 2x size of old array
+	memcpy(newarray, oldarray, *arraysize * 2 * sizeof(int));	// copy old array content to new array
 
 	*arraysize *= 2;
-	free(oldarray); 																					 // free old array from memory
+	free(oldarray); // free old array from memory
 	return newarray;
 }
 
 int main(){
 	char command;
 	int counter = 0;
-	int * collection = malloc(2*sizeof(int)); 								 // guessing size to be 2 ints
-	int insertions = 0; 																		   // keeps track of latest insertion in collection
+	int * collection = malloc(2*sizeof(int)); // guessing size to be 2 ints
+	int insertions = 0; // keeps track of latest insertion in collection
 	int size = sizeof(*collection)/sizeof(int);
 	int *psize = &size;
 
 	while(1){
-		command = getchar(); 																		 //read input char by char
+		command = getchar(); //read input char by char
 
 		if(command == 'a'){
 			if(insertions == *psize){ l
-				collection = dynamic_table(collection, psize);  		 // double collection size if full
+				collection = dynamic_table(collection, psize); // double collection size if full
 			}
-			collection[insertions] = counter; 										 // add counter to collection
-			insertions += 1; 																			 // insertion incremented to always point to next element
+			collection[insertions] = counter; // add counter to collection
+			insertions += 1; // insertion incremented to always point to next element
 		}
 		else if(command == 'b'){
 			//do nothing
 		}
 		else if(command == 'c'){
-			collection[insertions] = 0;														 // set this value to zero (which is never returned)
-			if(insertions == 0) {  																 // make sure that insertions is not pointing to element before 1st element
+			collection[insertions] = 0;	// set this value to zero (which is never returned)
+			if(insertions == 0) { // make sure that insertions is not pointing to element before 1st element
 			} else {
-				insertions -= 1;  																	 // make sure insertions is now pointing to the element that was just set to 0 which can be overwritten
+				insertions -= 1; // make sure insertions is now pointing to the element that was just set to 0 which can be overwritten
 			}
 		}
 		else{
@@ -53,6 +53,6 @@ int main(){
 			printf("\n");
 			break;
 		}
-		counter += 1; // Here the counter is incremented instead of doing it in each if 
+		counter += 1; // Here the counter is incremented instead of doing it in each if
 	}
 }
