f39ebe2c5d49ea2e5b0b0408b8d9e99c74b7a6c5
Frederik Rander Andersen <frederik396@gmail.com>
2018-09-27 22:38:35 +0200

    Update commandprogram.c

--- commandprogram.c
+++ commandprogram.c
@@ -1,52 +1,58 @@
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int buffer_size = 100; //guessing the buffersize (i.e. the size that needs to be allocated
-// int* buffer = malloc(buffer_size*sizeof(int)); 
+int *dynamic_table(int *oldarray, int *arraysize) {
+	int *newarray = NULL;
+	newarray = (int *) malloc(*arraysize * 2 * sizeof(int));   // new array = 2x size of old array
+	memcpy(newarray, oldarray, *arraysize * 2 * sizeof(int));	 // copy old array content to new array
 
-// how to dynamically allocate space? what type should buffer be?
-// use the dynamic tables (algo 2) 
-
-// when array gets full, new array of double size 
-// malloc the new array first and then free the old array space
+	*arraysize *= 2;
+	free(oldarray); 																					 // free old array from memory
+	return newarray;
+}
 
 int main(){
 	char command;
 	int counter = 0;
-	int collection[10] = {}; //what should be the size of the array ? think malloc
-	int insertions = 0; 
+	int * collection = malloc(2*sizeof(int)); 								 // guessing size to be 2 ints
+	int insertions = 0; 																		   // keeps track of latest insertion in collection
+	int size = sizeof(*collection)/sizeof(int);
+	int *psize = &size;
 
 	while(1){
-		command = getchar();	
-		
+		command = getchar(); 																		 //read input char by char
+
 		if(command == 'a'){
-			collection[insertions] = counter;
-			insertions += 1;
+			if(insertions == *psize){ l
+				collection = dynamic_table(collection, psize);  		 // double collection size if full
+			}
+			collection[insertions] = counter; 										 // add counter to collection
+			insertions += 1; 																			 // insertion incremented to always point to next element
 		}
 		else if(command == 'b'){
-			//Do nothing
+			//do nothing
 		}
 		else if(command == 'c'){
-			collection[insertions-1] = -1;
+			collection[insertions] = 0;														 // set this value to zero (which is never returned)
+			if(insertions == 0) {  																 // make sure that insertions is not pointing to element before 1st element
+			} else {
+				insertions -= 1;  																	 // make sure insertions is now pointing to the element that was just set to 0 which can be overwritten
+			}
 		}
 		else{
 			int i;
-			/* need to add logic which only prints 
-			the values that we want 
-			and not ALL the zeros */
-			for(i=0; i<(sizeof(collection)/sizeof(int)); i++){ 
-				if(collection[i] != -1){
-					printf("%i | ",collection[i]);
-				}
+			for(i=0; i<(*psize); i++){
+				if(i<1 || collection[i] != 	0) {
+						printf("%i | ",collection[i]); //if anything else than abc print whole collection
+					}
 				else {
 					//do nothing
 				}
 			}
+			printf("\n");
 			break;
 		}
-		counter += 1;
+		counter += 1; // Here the counter is incremented instead of doing it in each if 
 	}
-
 }
-
